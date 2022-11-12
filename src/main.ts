39ff14637485649c522c4f5f505ca6b5b58d25c2
kaikaibenkai <kaikai_miniworld@outlook.com>
2022-11-12 21:41:21 +0800

    feat: use VS Code plugin template

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Mnlua is now active!');
+}
+
+
+
+export function deactivate() {}
