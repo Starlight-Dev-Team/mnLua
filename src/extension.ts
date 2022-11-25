c7a1a0a7568c75e47d474ab887117d3741507d93
kaikaibenkai <kaikai_miniworld@outlook.com>
2022-11-25 22:28:37 +0800

    style: replace tabs to spaces

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,11 @@ import * as vscode from 'vscode';
 
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Mnlua is now active!');
-	let disposable = vscode.commands.registerCommand('mnlua.command', () => {
-		vscode.window.showInformationMessage('Hello, World!');
-	});
-	context.subscriptions.push(disposable);
+  console.log('Mnlua is now active!');
+  let disposable = vscode.commands.registerCommand('mnlua.command', () => {
+    vscode.window.showInformationMessage('Hello, World!');
+  });
+  context.subscriptions.push(disposable);
 }
 
 
