3718c81c70612283376eaf702bbdf2d385fde683
kaikaibenkai <kaikai_miniworld@outlook.com>
2022-11-25 22:20:16 +0800

    feat: fix version, add test command

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,10 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Mnlua is now active!');
+	let disposable = vscode.commands.registerCommand('mnlua.command', () => {
+		vscode.window.showInformationMessage('Hello, World!');
+	});
+	context.subscriptions.push(disposable);
 }
 
 
